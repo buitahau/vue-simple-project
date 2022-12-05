9c050321738c26b2d9a722961d2b546193417890
hau-kute <buitahau@gmail.com>
2022-12-05 22:28:39 +0700

    Initialize project

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue';
+
+import App from './App.vue';
+import BaseCard from './component/UI/BaseCard.vue';
+import BaseButton from './component/UI/BaseButton.vue';
+import BaseDialog from './component/UI/BaseDialog.vue';
+
+const app = createApp(App);
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-dialog', BaseDialog);
+
+app.mount('#app');
